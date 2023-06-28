a321cd22120c464e0413fe0eeaa8f3d3c941cf2a
iam7kei <iam7kei@gmail.com>
2023-06-28 16:09:37 +0800

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Weather and Venues App</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/vue.app.js') }}" defer></script>
